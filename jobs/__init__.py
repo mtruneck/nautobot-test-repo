b0d706770c245c31a8a907594e242c0b2c7b6eb4
Michal Trunecka <michal.trunecka@gmail.com>
2025-06-01 17:58:27 +0200

    Add Hello World Nautobot Job

---
+++ jobs/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the jobs directory a Python package
---
+++ jobs/hello_world.py
@@ -0,0 +1,39 @@
+from nautobot.apps.jobs import Job, StringVar
+from nautobot.core.celery import register_jobs
+
+
+class HelloWorldJob(Job):
+    """
+    A simple Hello World job that demonstrates the basic structure of a Nautobot job.
+    """
+    class Meta:
+        name = "Hello World"
+        description = "A simple job that says hello to the world"
+        commit_default = False
+
+    # Input variables for the job
+    name = StringVar(
+        description="Your name",
+        default="World",
+        required=False
+    )
+
+    def run(self, data=None, commit=None):
+        """
+        The main execution method of the job.
+        """
+        # Extract the name from the input data
+        name = data.get("name", "World")
+        
+        # Log a message that will be displayed in the job result
+        self.log_info(f"Hello, {name}!")
+        
+        # Return a success message
+        return f"Successfully said hello to {name}!"
+
+
+# Register the job with Nautobot
+jobs = [HelloWorldJob]
+
+# This line is required to register the job with Nautobot's job system
+register_jobs(*jobs)
