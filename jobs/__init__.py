96406e2f45c0e519af4940a620bee64420afc7a4
Michal Trunecka <michal.trunecka@gmail.com>
2025-06-06 11:48:23 +0200

    fix adding a new job

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,6 +1,7 @@
 # Import and register all job classes
 from nautobot.apps.jobs import register_jobs
 from .hello_world import HelloWorldJob, SemaphoreTaskRunner
+from .racom_ping import RacomDevicePing
 
 # Register all jobs
-register_jobs(HelloWorldJob, SemaphoreTaskRunner)
+register_jobs(HelloWorldJob, SemaphoreTaskRunner, RacomDevicePing)
