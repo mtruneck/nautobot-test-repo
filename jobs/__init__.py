fb1bbd6f802b899c5342c0d1d5da9d819bb3533b
Michal Trunecka <michal.trunecka@gmail.com>
2025-06-06 12:15:21 +0200

    try to fix the py init

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,7 +1,1 @@
 # Import and register all job classes
-from nautobot.apps.jobs import register_jobs
-from .hello_world import HelloWorldJob, SemaphoreTaskRunner
-from .racom_ping import RacomDevicePing
-
-# Register all jobs
-register_jobs(HelloWorldJob, SemaphoreTaskRunner, RacomDevicePing)
